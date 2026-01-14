1e58b0a40d685ec411bef8b9de10b725934e9a8b
octodecoy <octodecoy@gmail.com>
2026-01-14 09:45:57 -0700

    v1.7.3

--- Common/Source/Properties/AssemblyInfo.cs
+++ Common/Source/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")] // Leave static.
-[assembly: AssemblyFileVersion("1.6.2.0")] // Increment. 1/6/26
+[assembly: AssemblyFileVersion("1.6.3.0")] // Increment. 1/14/26
